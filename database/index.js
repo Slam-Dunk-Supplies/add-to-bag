cec4b41a7adb7a720037f632eab6ebd48145d317
Logan Rosenlund <loganrosenlund61@gmail.com>
2020-11-23 13:25:26 -0800

    Added port for database connection

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/items', {
+mongoose.connect('mongodb://localhost:27017/items', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
